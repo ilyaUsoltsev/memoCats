dc9cfd85fc987a3686db413b2915dc7f290bc380
ilya <Usoltsev@Ilyas-MacBook-Pro.local>
2018-07-20 22:55:53 +0300

    planning the js file

---
+++ script.js
@@ -0,0 +1,12 @@
+// get images. let's start with 3 different images + 1 cover= 7 total
+
+// function to display images randomly to the DOM
+
+// function to click on a face-down image and flip it
+
+// function to check if two images are the same
+
+//once all the images are face-up, get a win message on the screen
+
+//add timer
+
